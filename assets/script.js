72e4877a00116784ddb4c7ea63935463a68d8106
GabrielePagnottae <gabriele.pagnotta1998@gmail.com>
2022-11-11 18:33:37 +0100

    first commit

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+
+      hello:"https://www.venetoformazione.it/wp-content/uploads/2022/02/ottimizzare-immagini-display-retina.jpg",
+       images:[
+        {img:"https://www.venetoformazione.it/wp-content/uploads/2022/02/ottimizzare-immagini-display-retina.jpg"},
+        {img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHpTun6ZJzXtt0rIiRtvI5Hdt0JwtGFxxmcA&usqp=CAU"},
+      ],
+        
+      },
+
+      i:0,
+      
+  })
+
+ 
+
+ 
+
+  
+
+
+
+  
+
